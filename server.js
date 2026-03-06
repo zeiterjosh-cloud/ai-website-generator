3acb15cd7401548bfd04977f6b54493424494bde
zeiterjosh-cloud <zeiterjosh@gmail.com>
2026-03-06 17:47:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = __dirname; // Render-safe
 
 app.use(express.static(path.join(__dirname, "public")));
 
